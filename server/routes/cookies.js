1f58d5eccf8825636d9a33ddccb5fddaa55f74b0
dchatzinaki <61691396+dchatzinaki@users.noreply.github.com>
2022-04-13 14:46:37 +0100

    tidying up

--- server/routes/cookies.js
+++ server/routes/cookies.js
@@ -1,17 +1,19 @@
 module.exports = [{
   method: 'POST',
-  path: '/cookies',
+  path: '/',
   handler: (request, h) => {
     const cookies = request.payload.cookies;
     const hide = request.payload.hide;
     if (cookies === 'accept') {
-      return h.view('home', { cookies: 'accepted' })
+      return h.view('home', {
+        cookies: 'accepted', title: 'Add your reference for this export'
+      })
     }
     if (cookies === 'reject') {
-      return h.view('home', { cookies: "rejected" })
+      return h.view('home', { cookies: "rejected", title: 'Add your reference for this export' })
     }
     if (hide) {
-      return h.view('home', { hidden: true })
+      return h.view('home', { hidden: true, title: 'Add your reference for this export' })
     }
   }
 }]
